2327dda2ce53c3359b38f4cdd8ac370364cfc31e
admin <tnehdtlrl@naver.com>
2020-05-29 17:51:15 +0900

    0529

--- ProjectFirst/src/com/yedam/classes/morning/Code200529_1.java
+++ ProjectFirst/src/com/yedam/classes/morning/Code200529_1.java
@@ -30,47 +30,7 @@ public class Code200529_1 {
 			else {
 				System.out.print(comAry[k] + " ");
 			}
-		}
-		
-		System.out.println("야구게임 시작");
-		
-		while(equl) {
-		System.out.print("숫자을 입력하세요 : ");
-		Scanner scn = new Scanner(System.in);
-		
-		int[] userAry = new int[3];
-		for(int a=0; a<userAry.length; a++) {
-			try {
-			strike = scn.nextInt();
-			userAry[a] = strike;
-			if(strike > 0 || strike < 10) {
-				System.out.println("1~9의 숫자입력");
-			}
-		}catch(Exception e) {
-			System.out.println("숫자 범위가 아님");
-		}
-		for (int i=0; i<comAry.length; i++) { 
-			for (int j=0; j<userAry.length; j++) { 
-				if (comAry[i] == userAry[j] && i == j) { 
-					strike++; 
-				} 
-				else if (comAry[i] == userAry[j] && i != j) // 
-				{ 
-					ball++; 
-				} 
-			}
-		}
-		
-		System.out.println(strike + "스트라이크  " + ball + "볼"); 
-		if (strike == 3){ 
-		equl = false; 
-		} 
-		counter++; 
-		}
-		System.out.println("횟수 : " + counter); 
-		System.out.println("게임 끝"); 
-		
-		}
+		}		
 	}
 }
 
